63a768d400fe694fd62ca021d28e73e7ccd18e0b
Claudio Mignanti <c.mignanti@gmail.com>
2011-11-01 15:03:24 +0100

    Update README

--- AcmeBootProject/runit.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#make CHIP=at91sam9g20 BOARD=NetusG20 ORIGIN=sdcard DESTINATION=sdram OP_BOOTSTRAP_MCI=on TRACE_LEVEL=1 all
-python pizzica.py -f bin/boot-NetusG20-sdcard2sdram.bin -d /dev/ttyS0
-
